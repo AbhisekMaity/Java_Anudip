f41c1c0c142ea87a23a3e7c5c2c9dbb218439535
AbhisekMaity <109243534+AbhisekMaity@users.noreply.github.com>
2022-08-19 18:25:43 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,14 @@
+package com.java;
+
+public class EvenOdd {
+	public static void main(String args[]) {
+		int num=26;
+		if (num % 2 == 0) {
+			System.out.println("The number is even");
+		}
+		else {
+			System.out.println("The number is odd");
+		}
+	}
+
+}
