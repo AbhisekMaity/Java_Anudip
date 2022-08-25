0f30d97ed5f8abb6a5520f380dbabf6f33285f73
AbhisekMaity <109243534+AbhisekMaity@users.noreply.github.com>
2022-08-25 15:33:32 +0530

    Add files via upload

---
+++ 24-08-22(Assignment5)/WithOutMul.java
@@ -0,0 +1,24 @@
+package com.loop.java;
+import java.util.Scanner;
+
+/*
+ Write a Java program to multiply two given 
+ integers without using 
+ the multiply operator(*).
+ */
+public class WithOutMul {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner (System.in);
+		int a,b,i;
+		System.out.println("Enter the value of two numbers ");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		int mul=0;
+		for(i=1;i<=a;i++) {
+			mul=mul+b;
+		}
+			
+		System.out.println("The multiplication of two num is "+mul);
+		}
+
+	}
