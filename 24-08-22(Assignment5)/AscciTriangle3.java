9a86bf29b0fa64b23a084d9af7ec30e5900b493b
AbhisekMaity <109243534+AbhisekMaity@users.noreply.github.com>
2022-08-25 15:27:36 +0530

    Add files via upload

---
+++ 24-08-22(Assignment5)/AscciTriangle3.java
@@ -0,0 +1,18 @@
+package com.ascci.java;
+public class AscciTriangle3 {
+	public static void main(String args[]) {
+
+		for(int row=1;row<=4;row++) {
+			char ch ='A';
+
+			for(int col=1;col<=row;col++) {
+				System.out.print(ch+" ");
+				ch++;
+
+			}
+
+			System.out.println();
+		}
+	}
+
+}
