06f549a66c7fa58fe58978b80d11d9c58879c5eb
AbhisekMaity <109243534+AbhisekMaity@users.noreply.github.com>
2022-08-31 00:13:24 +0530

    Add files via upload

---
+++ 30-08-22(Assignment9)/MainMotoVehicle.java
@@ -0,0 +1,9 @@
+package com.OOPs.java;
+
+public class MainMotoVehicle {
+ public static void main(String args[]) {
+	 Carmoto odd = new Carmoto(10.0,"BMW","R@Z","Nexa234",100000);
+	 odd.display();
+	 odd.discount();
+ }
+}
---
+++ 30-08-22(Assignment9)/MotorVehicle.java
@@ -0,0 +1,31 @@
+/*
+ * 3.Create an abstract class MotorVehicle with the following details:
+Data Members:
+(a) modelName (b)modelNumber (c) modelPrice
+Methods:
+(a) display() to show all the details
+Create a subclass of this class Carthat inherits the class MotorVehicle and add the
+following details:
+Data Members:
+(b) discountRate
+Methods:
+(a) display() method to display the Car name, model number, price and the discount
+rate.
+(b) discount() method to compute the discount.
+ */
+package com.OOPs.java;
+
+abstract class MotorVehicle {
+	 String modelName;
+	 String modelNumber;
+	 double modelPrice;
+	 MotorVehicle(String modelName,String modelNumber,double modelPrice){
+		 this.modelName=modelName;
+		 this.modelNumber=modelNumber;
+		 this.modelPrice=modelPrice;
+	 }
+ public abstract void display() ;
+
+ 
+}
+ 
