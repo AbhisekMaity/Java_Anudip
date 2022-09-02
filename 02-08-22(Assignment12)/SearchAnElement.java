b34706d8b4d717ecb90b371b36a47032d731c855
AbhisekMaity <109243534+AbhisekMaity@users.noreply.github.com>
2022-09-02 12:18:50 +0530

    Add files via upload

---
+++ 02-08-22(Assignment12)/SearchAnElement.java
@@ -0,0 +1,34 @@
+
+package com.ds.java;
+import java.util.Scanner;
+public class SearchAnElement {
+ public static void main(String args[]) {
+	 Scanner sc = new Scanner(System.in);
+	 int size,i;
+	 System.out.println("Enter how many element you want to insert ");
+	 size=sc.nextInt();
+	 int arr[]=new int[size];
+	 System.out.println("Enter Array element one by one ");
+	 for(i=0;i<size;i++) {
+		 arr[i]=sc.nextInt();
+	 }
+	  System.out.println("Enter the element which you want to search ");
+	 int data=sc.nextInt();
+	  int flage=0;
+	 for(i=0;i<size;i++) {
+		 if(arr[i]==data) {
+			 flage=1;
+			 break;
+		 }
+		 
+	 }
+	 if(flage==1) {
+		  System.out.println(" The element is found at index " +i);
+
+	 }
+	 else {
+		  System.out.println(" The element is not found");
+
+	 }
+	 }
+}
