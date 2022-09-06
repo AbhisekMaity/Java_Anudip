3fa83d56b11f80e76bc40819c2b9d563979a65cc
AbhisekMaity <109243534+AbhisekMaity@users.noreply.github.com>
2022-09-06 20:35:40 +0530

    Add files via upload

---
+++ 06-09-22(Assignment14)/MainExamStuRes.java
@@ -0,0 +1,16 @@
+package com.OOPs.java;
+import java.util.Scanner;
+public class MainExamStuRes {
+  public static void main(String args[]) {
+	  Result obj=new Result();
+	  obj.name="Abhisek";
+	  obj.marks1=92;
+	  obj.marks2=91;
+	  obj.roll_no=145;
+	  obj.Percent_cal();
+	  obj.show();
+	  obj.display();
+	  
+  }
+  
+}
