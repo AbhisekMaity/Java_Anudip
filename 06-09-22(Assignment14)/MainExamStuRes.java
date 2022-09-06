68491180e2cbe956797e3a2af225275bf5e4e2c5
AbhisekMaity <109243534+AbhisekMaity@users.noreply.github.com>
2022-09-06 13:57:26 +0530

    Add files via upload

---
+++ 06-09-22(Assignment14)/MainExamStuRes.java
@@ -0,0 +1,15 @@
+package com.OOPs.java;
+//import java.util.Scanner;
+public class MainExamStuRes {
+  public static void main(String args[]) {
+	  Result obj=new Result();
+	  obj.name="Abhisek";
+	  obj.marks1=90;
+	  obj.marks2=95;
+	  obj.roll_no=121;
+	  obj.Percent_cal();
+	  obj.show();
+	  
+  }
+  
+}
---
+++ 06-09-22(Assignment14)/Result.java
@@ -0,0 +1,11 @@
+package com.OOPs.java;
+
+ class Result extends Students implements Exam {
+  public void Percent_cal() {
+	  double per;
+	  per=(marks1+marks2)/2;
+	System.out.println("The percentage marks is " +per);
+
+  }
+ }
+  
