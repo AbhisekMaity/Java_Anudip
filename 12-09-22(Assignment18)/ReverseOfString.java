ce4816a992014ba874a95946f29dcc9b7ccde2ba
AbhisekMaity <109243534+AbhisekMaity@users.noreply.github.com>
2022-09-12 11:17:45 +0530

    Add files via upload

--- 12-09-22(Assignment18)/ReverseOfString.java
+++ 12-09-22(Assignment18)/ReverseOfString.java
@@ -1,8 +1,11 @@
 package com.strings;
-
+import java.util.Scanner;
 public class ReverseOfString {
 	public static void main(String args[]) {
-		String s="Abhisek";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Sting");
+		
+		String s=sc.nextLine();
 		for(int i=s.length()-1;i>=0;i--) {
 			System.out.print(s.charAt(i));;
 		}
