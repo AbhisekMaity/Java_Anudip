4391af9f510ce42f2f16a86b3523dbe0aa6bc0cd
AbhisekMaity <109243534+AbhisekMaity@users.noreply.github.com>
2022-09-12 11:02:50 +0530

    Add files via upload

---
+++ 12-09-22(Assignment18)/ReverseOfString.java
@@ -0,0 +1,11 @@
+package com.strings;
+
+public class ReverseOfString {
+	public static void main(String args[]) {
+		String s="Abhisek";
+		for(int i=s.length()-1;i>=0;i--) {
+			System.out.print(s.charAt(i));;
+		}
+	}
+
+}
